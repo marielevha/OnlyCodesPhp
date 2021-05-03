33778480905bfc7e69e49c2c52e6d7634e06d799
Mariel Evha ABIR <mspeedy733@gmail.com>
2021-05-03 19:04:16 +0200

    Opérations

---
+++ src/operations.php
@@ -0,0 +1,21 @@
+<?php
+$a = 10;
+$b = 24;
+
+
+echo $a + $b;   // Somme de $a et $b.
+echo ("</br>"); // Saut de ligne
+
+echo $a - $b;	// Différence de $a et $b.
+echo ("</br>"); // Saut de ligne
+
+echo $a * $b;	// Multiplication de $a et $b.
+echo ("</br>"); // Saut de ligne
+
+echo $a / $b;	// Quotient de $a et $b.
+echo ("</br>"); // Saut de ligne
+
+echo $a % $b;	//  Reste de $a divisé par $b.
+echo ("</br>"); // Saut de ligne
+
+echo $a ** $b;	//Exponentiation Résultat de l'élévation de $a à la puissance $b.
